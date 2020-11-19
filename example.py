cf02d5757716f43dd9c86514463540dc0df744cc
helene-spring <74534349+helene-spring@users.noreply.github.com>
2020-11-19 10:49:24 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 def add(a, b):
 	return a+b
 
+def subtract(a,b):
+	return a-b
+
 
 def test_add():
 	assert add(2, 3) == 5
@@ -8,3 +11,8 @@ def test_add():
 
 def test_1():
 	assert 1==1
+	
+def test_subtract():
+	assert subtract(1,1) == 0
+	assert subtract(0,0) == 1
+	assert subtract(2,1) == 1
